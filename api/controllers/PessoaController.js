6727bb9408764477bd8fb3e87b3db17d46c7aadd
hugolopesxr <hugo_xr@hotmail.com>
2023-05-16 11:28:30 -0300

    first commit

---
+++ api/controllers/PessoaController.js
@@ -0,0 +1,62 @@
+const { where } = require('sequelize')
+const database = require('../models')
+
+class PessoaController {
+    // Buscar todas as pessoas no registro
+    static async pegaTodasAsPessoas(req, res) {
+        try {
+            const todasAsPessoas = await database.Pessoas.findAll()
+            return res.status(200).json(todasAsPessoas)
+        } catch (console) {
+            return res.status(500).json(error.message)
+        }
+    }
+
+    // buscar apenas uma unica pessoa no registro
+    static async pegaUmaPessoa(req, res) {
+        const { id } = req.params
+        try {
+            const umaPessoa = await database.Pessoas.findOne({ where: { id: Number(id) } })
+            return res.status(200).json(umaPessoa)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async criaPessoa(req, res) {
+        const novaPessoa = req.body
+        try {
+            const novaPessoaCriada = await database.Pessoas.create(novaPessoa)
+            return res.status(200).json(novaPessoaCriada)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
+    //atualizar um registro
+    static async atualizaPessoa(req, res) {
+        const { id } = req.params
+        const novasInfos = req.body
+        try {
+            await database.Pessoas.update(novasInfos, { where: { id: Number(id) } })
+            const pessoaAtualizada = await database.Pessoas.findOne({ where: { id: Number(id) } })
+            return res.status(200).json(pessoaAtualizada)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
+    //deletar um registro
+    static async apagaPessoa(req, res) {
+        const { id } = req.params
+        try {
+        await database.Pessoas.destroy({ where: { id: Number(id) } })
+        return res.status(200).json({ mensagem: `id ${id} deletado com sucesso.` })
+        } catch (error){
+            return res.status(500).json(error.message)
+        }
+    }
+
+}
+
+module.exports = PessoaController
